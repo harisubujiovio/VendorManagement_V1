0712418ad5855a3f64e046fba4e2b35fa7cabeb9
Harihara <harisubujiovio@gmail.com>
2022-12-18 11:18:43 +0530

    Enable Authorize Button in Swagger API

--- VendorMangement.API/Program.cs
+++ VendorMangement.API/Program.cs
@@ -8,6 +8,7 @@ using IAuthenticationService = VendorMangement.API.Services.Authentication.IAuth
 using AuthenticationService = VendorMangement.API.Services.Authentication.AuthenticationService;
 using System.Runtime.CompilerServices;
 using VendorManagement.DBclient.DBProvider;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -16,8 +17,34 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
+    //builder.Services.AddSwaggerGen();
+    builder.Services.AddSwaggerGen(c => {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "JWTToken_Auth_API",
+            Version = "v1"
+        });
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+        });
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+    });
     //builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection(JwtSettings.SectionName));
 
     //builder.Services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
